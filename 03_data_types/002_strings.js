4184830032268d4cf008bd6b1b6f442d564df1e7
Khaled Allam <kaaoaa505@gmail.com>
2023-07-03 14:56:06 +0300

    Strings data type methods

---
+++ 03_data_types/002_strings.js
@@ -0,0 +1,14 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+
+let hello = 'Hello!.';
+
+console.log("---- hello is: ", hello);
+console.log(`---- hello is: ${hello}`);
+console.log("---- typeof hello is: ", typeof hello);
+console.log("---- hello.length is: ", hello.length);
+console.log("---- hello.toUpperCase() is: ", hello.toUpperCase());
+console.log("---- hello.toLowerCase() is: ", hello.toLowerCase());
+
+console.log("---- 'Keyword'==='Keyword' is: ", 'Keyword'==='Keyword');
+console.log("---- 'Keyword'==='keyword' is: ", 'Keyword'==='keyword');
+console.log("---- 'Keyword'.toLowerCase()==='keyword' is: ", 'Keyword'.toLowerCase()==='keyword');
