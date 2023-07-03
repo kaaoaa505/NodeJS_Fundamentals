bce748e63eb5a87177bed4ce68a812a34cfa9503
Khaled Allam <kaaoaa505@gmail.com>
2023-07-03 14:16:53 +0300

    Numbers incorrect cases

---
+++ 03_data_types/001_numbers.js
@@ -0,0 +1,17 @@
+let x = 88;
+
+console.log("---- typeof x is: ", typeof x);
+
+let pi = 3.14159;
+
+console.log("---- typeof pi is: ", typeof pi);
+
+console.log("---- Binary \t 0b0101110 is: ", 0b0101110);
+console.log("---- Hex \t 0x0101110 is: ", 0x0101110);
+console.log("---- Octet \t 0o0101110 is: ", 0o0101110);
+
+console.log("---- Incorrect  \t 0.1 + 0.7 is: ", 0.1 + 0.7);
+console.log("---- Incorrect  \t 987654321123456789 is: ", 987654321123456789);
+console.log("---- Incorrect  \t Math.pow(10, 999) is: ", Math.pow(10, 999));
+console.log("---- Incorrect  \t Math.pow(-10, 999) is: ", Math.pow(-10, 999));
+console.log("---- Incorrect  \t 'hello'*5 is: ", 'hello'*5);
