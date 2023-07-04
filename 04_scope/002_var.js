e7044a6b3fd01bebeab1933cffc78bb609cb5816
Khaled Allam <kaaoaa505@gmail.com>
2023-07-04 10:26:49 +0300

    var scope

--- 04_scope/002_var.js
+++ 04_scope/002_var.js
@@ -1,8 +1,8 @@
-let x = 1;
-let y = 1;
+var x = 1;
+var y = 1;
 
 if (true) {
-  let x = 5;
+  var x = 5;
   y = 5;
   console.log("---- x is: ", x);
   console.log("---- typeof x is: ", typeof x);
@@ -19,7 +19,7 @@ console.log("---- y is: ", y);
 console.log("---- typeof y is: ", typeof y);
 console.log("\n\n");
 
-let num = 8;
+var num = 8;
 function test() {
   function innerTest() {
     console.log("---- num is: ", num);
@@ -33,8 +33,8 @@ test();
 
 console.log("\n\n");
 
-let index = 999;
-for (let index = 0; index < 10; index++) {
+var index = 999;
+for (var index = 0; index < 10; index++) {
   console.log("---- index in for is: ", index);
 }
 console.log("---- index out of for is: ", index);
