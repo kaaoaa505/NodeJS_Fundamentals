e3e4bc7a349d24b7fd02d8dcb7669003a63837dc
Khaled Allam <kaaoaa505@gmail.com>
2023-07-04 09:38:22 +0300

    BigInt data type

---
+++ 03_data_types/007_big_int.js
@@ -0,0 +1,13 @@
+let bigInt = 12345678900987654321n;
+
+console.log("---- bigInt is: ", bigInt);
+console.log("---- typeof bigInt is: ", typeof bigInt);
+console.log("\n\n");
+
+bigInt = bigInt * 10000000000000000000n;
+
+console.log("---- bigInt is: ", bigInt);
+console.log("---- typeof bigInt is: ", typeof bigInt);
+console.log("---- 'Hello'+bigInt is: ", 'Hello'+bigInt);
+console.log("---- 2n*bigInt is: ", 2n*bigInt);
+console.log("\n\n");
