6931552c83b3b26bdc2b35f531ac8f932e4f972e
Khaled Allam <kaaoaa505@gmail.com>
2023-07-04 15:09:46 +0300

    string slice

---
+++ 08_strings/002_slice.js
@@ -0,0 +1,11 @@
+let myStr = "Hello World.";
+
+console.log("---- myStr is: ", myStr);
+console.log("---- myStr.length is: ", myStr.length);
+console.log("---- typeof myStr is: ", typeof myStr);
+console.log("---- myStr.slice(1, 10) is: \t\t", myStr.slice(1, 10));
+console.log("---- myStr.slice(1, 12) is: \t\t", myStr.slice(1, 12));
+console.log("---- myStr.slice(1, myStr.length) is: \t ", myStr.slice(1, myStr.length));
+console.log("---- myStr.slice(1, -1) is: \t\t ", myStr.slice(1, -1));
+console.log("---- myStr.slice(1, -2) is: \t\t ", myStr.slice(1, -2));
+console.log("\n\n");
