d67f45ae7e8ddfb16b86022dd263ac86bf7ef114
Khaled Allam <kaaoaa505@gmail.com>
2023-07-04 15:04:11 +0300

    String includes, charAt, and indexOf methods

---
+++ 08_strings/001_includes.js
@@ -0,0 +1,12 @@
+let myStr = "Hello World.";
+
+console.log("---- myStr is: ", myStr);
+console.log("---- typeof myStr is: ", typeof myStr);
+console.log("---- myStr.includes('h') is: ", myStr.includes('h'));
+console.log("---- myStr.includes('H') is: ", myStr.includes('H'));
+console.log("---- myStr.charAt(0) is: ", myStr.charAt(0));
+console.log("---- myStr.indexOf('W') is: ", myStr.indexOf('W'));
+console.log("---- myStr.indexOf('ll') is: ", myStr.indexOf('ll'));
+console.log("---- myStr.indexOf('o') is: ", myStr.indexOf('o'));
+console.log("---- myStr.lastIndexOf('o') is: ", myStr.lastIndexOf('o'));
+console.log("\n\n");
