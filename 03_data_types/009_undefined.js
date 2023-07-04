1ab9f8ab480df8f8d2822dd3114d3a4570baee6f
Khaled Allam <kaaoaa505@gmail.com>
2023-07-04 09:57:49 +0300

    Undefined data type

---
+++ 03_data_types/009_undefined.js
@@ -0,0 +1,20 @@
+let x;
+
+console.log('---- x is: ', x);
+console.log('---- typeof x is: ', typeof x);
+console.log('\n\n');
+
+let fun = function(){};
+
+console.log('---- fun is: ', fun);
+console.log('---- fun() is: ', fun());
+console.log('---- typeof fun is: ', typeof fun);
+console.log('\n\n');
+
+let person = {};
+
+console.log('---- person is: ', person);
+console.log('---- person.name is: ', person.name);
+console.log('---- person.age is: ', person.age);
+console.log('---- typeof person is: ', typeof person);
+console.log('\n\n');
