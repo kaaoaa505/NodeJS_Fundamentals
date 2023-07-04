55993210eb54cf9e2eb03e060b336a034da87ca6
Khaled Allam <kaaoaa505@gmail.com>
2023-07-04 14:10:45 +0300

    Destruct object to get single values

---
+++ 07_objects/002_destruct_object.js
@@ -0,0 +1,13 @@
+const person = {
+    name: 'Khaled Allam',
+    age: 34,
+    job: 'Senior Full Stack Software Engineer'
+};
+
+const {name, age, job} = person;
+
+console.log("---- person is: ", person);
+console.log("---- person.name is: ", person.name);
+console.log("---- person.age is: ", person.age);
+console.log("---- person.job is: ", person.job);
+console.log("\n\n");
