82244835cb0570261f7fffc76350eea91429c675
Khaled Allam <kaaoaa505@gmail.com>
2023-07-06 09:24:31 +0300

    fs sync, callback, promise and async with await.

---
+++ 10_fs/001_basics.cjs
@@ -0,0 +1,24 @@
+let fs = require("fs");
+
+let stats = fs.statSync('./package.json');
+console.info("---- Sync stats.size in bytes is: ", stats.size);
+
+fs.stat("./package.json", (err, stats) => {
+  if (err) {
+    console.error("---- err is: ", err);
+  } else {
+    console.info("---- Callback stats.size in bytes is: ", stats.size);
+  }
+});
+
+let statPromise = fs.promises.stat('./package.json');
+
+statPromise.then(stats => {
+  console.info("---- Promise stats.size in bytes is: ", stats.size);
+});
+
+let statAsyncFunction = async () => {
+  let statAsync = await fs.promises.stat('./package.json');
+  console.info("---- async await statAsync.size in bytes is: ", statAsync.size);
+}
+statAsyncFunction();
