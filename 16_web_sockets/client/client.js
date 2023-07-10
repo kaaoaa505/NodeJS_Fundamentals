4ecd1525add228da1153d24883854e437d8f96b7
Khaled Allam <kaaoaa505@gmail.com>
2023-07-10 11:05:09 +0300

    Emit event from server and listen on client, Emit event from client and listen on server.

--- 16_web_sockets/client/client.js
+++ 16_web_sockets/client/client.js
@@ -1,6 +1,15 @@
-import { io } from 'socket.io-client';
+import { io } from "socket.io-client";
 
-const SERVER_URL = 'http://localhost:8080';
+const SERVER_URL = "http://localhost:8080";
 const socket = io(SERVER_URL);
 
 console.log(`Client connected to: ${socket.io.uri}`);
+
+socket.on("greeting_from_server_to_client_event", (data) => {
+  console.log("Client socket on greeting_from_server_to_client_event console log");
+  console.log("---- Client data is: ", data);
+});
+
+for (let index = 0; index < 5; index++) {
+  socket.emit("greeting_from_client_to_server_event", "Client socket emit message .... " + index);
+}
--- 16_web_sockets/server/server.js
+++ 16_web_sockets/server/server.js
@@ -1,15 +1,31 @@
-import express from 'express';
-import { Server } from 'socket.io';
-import http from 'http';
+import express from "express";
+import { Server } from "socket.io";
+import http from "http";
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
-const server = http.createServer(app)
+const server = http.createServer(app);
 const io = new Server(server);
 
 console.log(io.path());
 
+io.on("connection", (socket) => {
+  console.log("A new client has connected!.");
+
+  for (let index = 0; index < 10; index++) {
+    socket.emit(
+      "greeting_from_server_to_client_event",
+      "Server io on socket emit message .... " + index
+    );
+  }
+
+  socket.on("greeting_from_client_to_server_event", (data) => {
+    console.log("Server io on connection socket on greeting_from_client_to_server_event console log");
+    console.log("---- Server data is: ", data);
+  });
+});
+
 server.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
