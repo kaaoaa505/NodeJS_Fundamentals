fcc403d5d406c304a58a174ba090719b02c45fcd
Khaled Allam <kaaoaa505@gmail.com>
2023-07-11 13:32:26 +0300

    Post request json body parse data

--- 20_server_framework/src/KallamServer.js
+++ 20_server_framework/src/KallamServer.js
@@ -9,24 +9,42 @@ export default {
       const { pathname, query: queryString } = urlParser.parse(url);
       const query = qs.parse(queryString);
 
-      const route = routes.find((r) => {
-        return r.method === method && r.path === pathname;
+      let data = "";
+      request.on("data", (chunk) => {
+        data += chunk;
       });
 
-      if (route) {
-        const routeObj = {
-          request,
-          response,
-          query,
-        };
+      request.on("end", () => {
+        let body;
 
-        const { statusCode, data } = route.onRequest(routeObj);
-        response.writeHead(statusCode);
-        response.end(data);
-      } else {
-        response.writeHead(404);
-        response.end(`Incorrect route ${method} ${pathname}`);
-      }
+        try {
+          body = JSON.parse(data);
+        } catch (e) {
+          // console.log('The request body is not a valid json');
+          // console.error("Error", e);
+          body = {};
+        }
+
+        const route = routes.find((r) => {
+          return r.method === method && r.path === pathname;
+        });
+
+        if (route) {
+          const routeObj = {
+            request,
+            response,
+            query,
+            body,
+          };
+
+          const { statusCode, data } = route.onRequest(routeObj);
+          response.writeHead(statusCode);
+          response.end(data);
+        } else {
+          response.writeHead(404);
+          response.end(`Incorrect route ${method} ${pathname}`);
+        }
+      });
     });
 
     return {
--- 20_server_framework/src/routes.js
+++ 20_server_framework/src/routes.js
@@ -1,18 +1,35 @@
-function handleHomeRequest(routeObj){
-    console.log('✅ routeObj.query    ', routeObj.query)
-    
+function handleHomeRequest(routeObj) {
+    console.log("✅ routeObj.query    ", routeObj.query);
+  
     return {
-        statusCode: 200,
-        data: 'Welcome to home page.'
-    }
-}
+      statusCode: 200,
+      data: "Welcome to home page.",
+    };
+  }
+  
+  function handleTestRequest(routeObj) {
+    console.log("✅ routeObj.body    ", routeObj.body);
+  
+    return {
+      statusCode: 200,
+      data: "Just for test POST API request.",
+    };
+  }
 
-export default [{
-    method: 'GET',
-    path: '/home',
-    onRequest: handleHomeRequest
-},{
-    method: 'GET',
-    path: '/',
-    onRequest: handleHomeRequest
-}];
+export default [
+  {
+    method: "GET",
+    path: "/home",
+    onRequest: handleHomeRequest,
+  },
+  {
+    method: "GET",
+    path: "/",
+    onRequest: handleHomeRequest,
+  },
+  {
+    method: "POST",
+    path: "/test",
+    onRequest: handleTestRequest,
+  },
+];
